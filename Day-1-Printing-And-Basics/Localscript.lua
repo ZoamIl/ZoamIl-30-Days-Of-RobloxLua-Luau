79e1c6dc99b1a0927c2b6908895c9a8972abf2f9
ZoamIl <104103312+ZoamIl@users.noreply.github.com>
2023-10-30 23:16:43 +0200

    Create Localscript.lua

---
+++ Day-1-Printing-And-Basics/Localscript.lua
@@ -0,0 +1,26 @@
+--INTRODUCTION--
+A Localscript is a special type of script that runs on the player's device in a game. 
+
+It is specifically designed to control and manipulate the behavior of objects that the player can see. 
+
+Developers use Localscripts to create custom interactions, animations, and gameplay mechanics. 
+
+These scripts are executed on the client-side, which means they are responsible for rendering graphics and handling user input.
+
+Localscripts have the ability to access and modify objects that are visible and interactive to the player, such as characters, parts, and user interfaces. 
+
+This allows developers to create dynamic and interactive experiences for players.
+
+For instance, consider a localscript that changes the color of a part when the player clicks on it:
+
+local part = script.Parent
+
+part.MouseButton1Click:Connect(function()
+    part.BrickColor = BrickColor.new("Bright red")
+end)
+
+In summary, Localscripts are essential tools for developers in application and game development. 
+
+They enable developers to control the behavior of objects on the client-side, resulting in custom interactions, animations, and mechanics. 
+
+Ultimately, Localscripts play a crucial role in crafting captivating and interactive experiences for users.
