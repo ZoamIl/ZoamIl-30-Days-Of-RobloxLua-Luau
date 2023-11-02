3b3e1a8c6c0ed63c670375326e8e33935adbfbbe
ZoamIl <104103312+ZoamIl@users.noreply.github.com>
2023-11-02 14:47:11 +0200

    Update and rename Functions.lua.txt to Functions1.lua

---
+++ Day-4-Functions-And-Events/Functions1.lua
@@ -0,0 +1,45 @@
+--INTRODUCTION--
+Functions play a crucial role in RobloxLua (Luau) scripting, enabling developers to create reusable blocks of code that perform specific tasks. 
+
+In this essay, we will explore the importance of functions and how they enhance the functionality of Roblox projects.
+
+--LEARNING--
+Functions in RobloxLua (Luau) scripting allow us to encapsulate a set of instructions and execute them whenever needed. 
+
+They enable us to organize our code, improve efficiency, and promote code reusability.
+
+To illustrate the concept of functions, let's consider the following example:
+
+function ChangeColor()
+  local Functionpart = game.workspace.part
+  Functionpart.BrickColor = BrickColor.random()
+end
+
+wait(1)
+
+ChangeColor()
+
+
+In this script, we have defined a function called "ChangeColor". 
+  
+This function retrieves a specific part from the workspace, and then changes its brick color to a random color using the BrickColor.random() function.
+
+By calling the ChangeColor() function after waiting for 1 second using the wait(1) function, we can trigger the color change in our Roblox project.
+
+Functions provide a way to encapsulate a set of instructions and execute them at a specific time or under certain conditions. 
+        
+In this example, the ChangeColor() function allows us to easily change the color of a specific part in our game.
+
+By utilizing functions, we can create dynamic and interactive experiences in our projects. 
+          
+We can reuse the ChangeColor() function whenever we need to modify the color of a different part, promoting code reusability and reducing redundancy.
+
+In summary, functions in RobloxLua (Luau) scripting provide a powerful tool for creating modular and reusable code. 
+            
+They enhance the functionality of our projects by allowing us to encapsulate specific tasks and execute them whenever needed.
+
+Functions promote code organization, efficiency, and code reusability, making our scripts more maintainable and easier to understand.
+
+Now that you have a grasp of how functions work, you can leverage their capabilities effectively in your RobloxLua (Luau) scripts.
+
+--NOW YOU KNOW HOW FUNCTIONS(1) WORK--
