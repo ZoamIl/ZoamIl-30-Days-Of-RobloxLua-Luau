f3f4e38810d6df9fd7645353e477c51300711014
ZoamIl <104103312+ZoamIl@users.noreply.github.com>
2023-11-02 15:00:53 +0200

    Update Functions2.lua

--- Day-4-Functions-And-Events/Functions2.lua
+++ Day-4-Functions-And-Events/Functions2.lua
@@ -1,4 +1,24 @@
 --EXTRA--
 If we want to assign Functions to different parts in one script we can do this:
 
-    
+function ChangeColor()
+  Functionpart.BrickColor = BrickColor.random()
+end
+
+wait(1)
+
+ChangeColor(local Functionpart = game.Workspace.Part)
+
+wait(1)
+
+ChangeColor(local Functionpart = game.Workspace.Part2)
+
+wait(1)
+
+ChangeColor(local Functionpart = game.Workspace.Part3)
+
+In here, we remove the second line because we're assigning the Function to 3 different parts.
+
+Now, we call the 3 parts when we call the Function.
+
+--NOW YOU KNOW THE EXTRA OF FUNCTIONS--
